a4fdbf270ad6790144d51fe61d69ae711dbeb953
Jim Saari <jim.saari97@gmail.com>
2020-12-21 09:17:45 +0200

    Add controls component

---
+++ src/components/Controls.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="flex items-center py-6">
+    <h1 v-if="title" class="text-xl font-semibold">{{ title }}</h1>
+    <figure class="ml-auto">
+      <button
+        type="button"
+        @click="newNote"
+        class="py-2 px-6 bg-blue-500 rounded-md text-gray-50 hover:bg-blue-600"
+      >
+        New
+      </button>
+    </figure>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Controls",
+  props: {
+    title: String,
+  },
+  methods: {
+    newNote() {
+      this.$store.commit("newNote");
+    },
+  },
+};
+</script>
