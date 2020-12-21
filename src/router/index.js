1dc307b6c7a118324c1c79a791cc819924a6473c
Jim Saari <jim.saari97@gmail.com>
2020-12-21 09:14:07 +0200

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,21 +8,21 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/settings",
+    name: "Settings",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "settings" */ "../views/Settings.vue"),
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
